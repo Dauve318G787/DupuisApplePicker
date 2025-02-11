4f1a4415f87959b0d80cb17b0117f7b432355485
Dauve318G787 <davbekdup@gmail.com>
2025-02-11 12:42:39 -0600

    Add initial asset scripts

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that AppleTree will change directions
+    public float changeDirChance = 0.1f;
+
+    //Seconds between Apple instantiations
+    public float appleDropDelay = 1f;
+
+    void Start() {
+        //Start dropping apples
+    }
+
+    void Update() {
+        //Basic movement
+        //Change directions
+    }
+}
