ed670cac03b7bbe683bd258441f7f9f8b0a3f4ce
Dauve318G787 <davbekdup@gmail.com>
2025-02-11 15:30:33 -0600

    Project now keeps score

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,34 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;  // No changes needed here, we're using TextMeshPro components in ScoreCounter
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public ScoreCounter scoreCounter;  // Reference to ScoreCounter component for score management
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Find the ScoreCounter GameObject and get its ScoreCounter script component
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        if (scoreGO != null)
+        {
+            scoreCounter = scoreGO.GetComponent<ScoreCounter>();
+        }
+        else
+        {
+            Debug.LogError("ScoreCounter GameObject not found in the scene.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Get current screen position of mouse from input
+        // Get the current screen position of the mouse
         Vector3 mousePos2D = Input.mousePosition;
 
+        // Set the z value to be negative of the camera's position to match world space
         mousePos2D.z = -Camera.main.transform.position.z;
 
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
+        // Convert the 2D mouse position to 3D world position
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 
+        // Update the basket position to follow the mouse
         Vector3 pos = this.transform.position;
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
 
-    void OnCollisionEnter( Collision coll ) {
+    // Called when the basket collides with another object
+    void OnCollisionEnter(Collision coll)
+    {
         GameObject collidedWith = coll.gameObject;
-        if( collidedWith.CompareTag("Apple")) {
-            Destroy( collidedWith );
+
+        // If the basket collides with an object tagged as "Apple", destroy the apple and update the score
+        if (collidedWith.CompareTag("Apple"))
+        {
+            Destroy(collidedWith);  // Destroy the apple
+            scoreCounter.score += 100;  // Increment the score by 100
+            Debug.Log("Apple collided! Score: " + scoreCounter.score);  // Debug log for score change
         }
     }
 }
