ecbeb47b44eb59001723e97d555303bd9c166abe
Dauve318G787 <davbekdup@gmail.com>
2025-02-12 23:02:26 -0600

    Apple Picker tutorial in Bond book is finished

--- Assets/HighScore.cs
+++ Assets/HighScore.cs
@@ -5,29 +5,47 @@ using UnityEngine.UI;
 using TMPro;
 public class HighScore : MonoBehaviour
 {
-
     static private TextMeshProUGUI _UI_TEXT;
     static private int _SCORE = 1000;
 
-    private TextMeshProUGUI txtCom;
-
     void Awake() {
-        _UI_TEXT = GetComponent<TextMeshProUGUI>();
+        if (_UI_TEXT == null) {
+            _UI_TEXT = GetComponent<TextMeshProUGUI>();  // Only set _UI_TEXT if it's not already set
+        }
+
+        if (PlayerPrefs.HasKey("HighScore")) {
+            SCORE = PlayerPrefs.GetInt("HighScore");
+        }
+
+        PlayerPrefs.SetInt("HighScore", SCORE);
     }
 
     static public int SCORE {
         get { return _SCORE; }
         private set {
             _SCORE = value;
-            if ( _UI_TEXT != null ) {
+            PlayerPrefs.SetInt("HighScore", value);
+            if (_UI_TEXT != null) {
                 _UI_TEXT.text = "High Score: " + value.ToString("#,0");
+            } else {
+                Debug.LogError("UI Text is null. Make sure HighScore is initialized correctly.");
             }
         }
     }
 
-    static public void TRY_SET_HIGH_SCORE( int scoreToTry ) {
-        if (scoreToTry <= SCORE ) return;
+    static public void TRY_SET_HIGH_SCORE(int scoreToTry) {
+        if (scoreToTry <= SCORE) return;
         SCORE = scoreToTry;
     }
 
+    public bool resetHighScoreNow = false;
+
+    void OnDrawGizmos() {
+        if (resetHighScoreNow) {
+            resetHighScoreNow = false;
+            PlayerPrefs.SetInt("HighScore", 1000);
+            Debug.LogWarning("PlayerPrefs HighScore reset to 1,000");
+        }
+    }
 }
+
